2799d2470da663474220ac184e26166ca422360a
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-06-09 13:21:32 +0700

    Initial Commit

---
+++ TaskNest-Task-Manager-SwiftUI/TaskNest_Task_Manager_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskNest_Task_Manager_SwiftUIApp.swift
+//  TaskNest-Task-Manager-SwiftUI
+//
+//  Created by Akbar Eka Putra on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskNest_Task_Manager_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
