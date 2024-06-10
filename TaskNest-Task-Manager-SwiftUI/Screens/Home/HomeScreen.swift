f4674bc4ccfad7f624d89e618905104dd80181c8
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-06-10 20:29:11 +0700

    feat: add Today's Task Card View

--- TaskNest-Task-Manager-SwiftUI/Screens/Home/HomeScreen.swift
+++ TaskNest-Task-Manager-SwiftUI/Screens/Home/HomeScreen.swift
@@ -11,42 +11,89 @@ struct HomeScreen: View {
   var body: some View {
     NavigationView {
       ZStack {
-        VStack(alignment: .leading) {
-          Text("Top Priority Tasks")
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .font(.title3)
-            .fontWeight(.semibold)
-          ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 16) {
-              TopPriorityTasksCardView(
-                category: "work", task: "Meeting with clients", time: "19 Mar, 2024 at 7:30 PM")
-              TopPriorityTasksCardView(
-                category: "education", task: "Meeting with clients", time: "19 Mar, 2024 at 7:30 PM"
-              )
-              TopPriorityTasksCardView(
-                category: "health", task: "Meeting with clients", time: "19 Mar, 2024 at 7:30 PM")
-              TopPriorityTasksCardView(
-                category: "home", task: "Meeting with clients", time: "19 Mar, 2024 at 7:30 PM")
-            }
-            .frame(height: 170)
-            .padding(.leading, 3)
-          }
+        ScrollView(.vertical, showsIndicators: false) {
+          VStack(alignment: .leading) {
 
-          Spacer()
-        }
-        .padding()
-      }
-      .navigationBarTitle("Home", displayMode: .inline)
-      .navigationBarItems(
-        trailing:
-          Button(action: {
-            print("notif button clicked")
-          }) {
-            Image(systemName: "bell")
-              .outlineButtonStyle()
-              .notifBadgeExt(count: 9)
+            Text("Top Priority Tasks")
+              .padding(.leading)
+              .frame(maxWidth: .infinity, alignment: .leading)
+              .font(.title3)
+              .fontWeight(.semibold)
+            ScrollView(.horizontal, showsIndicators: false) {
+              HStack {
+                TopPriorityTasksCardView(
+                  category: "work", task: "Meeting with clients", time: "19 Mar, 2024 at 7:30 PM")
+                TopPriorityTasksCardView(
+                  category: "education", task: "Meeting with clients",
+                  time: "19 Mar, 2024 at 7:30 PM"
+                )
+                TopPriorityTasksCardView(
+                  category: "health", task: "Meeting with clients", time: "19 Mar, 2024 at 7:30 PM")
+                TopPriorityTasksCardView(
+                  category: "home", task: "Meeting with clients", time: "19 Mar, 2024 at 7:30 PM")
+              }
+              .frame(height: 170)
+              .padding(.leading, 3)
+              .padding(.trailing, 10)
+            }
+
+            VStack(spacing: 20) {
+              HStack {
+                Text("Today's Tasks")
+                  .font(.title3)
+                  .fontWeight(.semibold)
+
+                Spacer()
+
+                Text("See all")
+                  .font(.system(size: 16))
+                  .foregroundColor(.black.opacity(0.5))
+              }
+              .padding(.top, 10)
+              TodaysTasksCardView(
+                taskCategory: "work",
+                taskTitle: "Preparations",
+                taskDescription:
+                  "Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate.",
+                taskTime: "7:30 AM - 8:00 AM")
+              TodaysTasksCardView(
+                taskCategory: "education",
+                taskTitle: "Preparations",
+                taskDescription:
+                  "Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate.",
+                taskTime: "7:30 AM - 8:00 AM")
+              TodaysTasksCardView(
+                taskCategory: "home",
+                taskTitle: "Preparations",
+                taskDescription:
+                  "Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate.",
+                taskTime: "7:30 AM - 8:00 AM")
+              TodaysTasksCardView(
+                taskCategory: "work",
+                taskTitle: "Preparations",
+                taskDescription:
+                  "Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate.",
+                taskTime: "7:30 AM - 8:00 AM")
+            }
+            .padding(.horizontal)
           }
-      )
+        }
+
+        .navigationBarTitle("Home", displayMode: .inline)
+        .navigationBarItems(
+          trailing:
+            Button(action: {
+              print("notif button clicked")
+            }) {
+              Image(systemName: "bell")
+                .outlineButtonStyle()
+                .notifBadgeExt(count: 90)
+                .padding(.bottom, 10)
+            }
+
+        )
+
+      }
     }
   }
 }
--- TaskNest-Task-Manager-SwiftUI/Views/Cards/TodaysTasksCardView.swift
+++ TaskNest-Task-Manager-SwiftUI/Views/Cards/TodaysTasksCardView.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct TodaysTasksCardView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var taskCategory: String
+  var taskTitle: String
+  var taskDescription: String
+  var taskTime: String
+
+  var body: some View {
+    VStack {
+      HStack {
+        Image(
+          systemName: taskCategory == "work"
+            ? "handbag.fill"
+            : taskCategory == "home"
+              ? "house.fill"
+              : taskCategory == "finance"
+                ? "dollarsign"
+                : taskCategory == "education"
+                  ? "book.fill"
+                  : taskCategory == "health"
+                    ? "figure.disc.sports"
+                    : "camera.metering.unknown"
+        )
+        .resizable()
+        .frame(width: 25, height: 25)
+        .scaledToFit()
+        .padding(20)
+        .background(.gray.opacity(0.1))
+        .cornerRadius(12)
+        VStack(alignment: .leading, spacing: 3) {
+          Text(taskTitle)
+            .fontWeight(.bold)
+          Text(taskDescription)
+            .foregroundColor(.isDark.opacity(0.5))
+            .font(.system(size: 14))
+          Text(taskTime)
+            .foregroundColor(.accent)
+            .fontWeight(.semibold)
+            .font(.system(size: 14))
+        }
+      }
+      .padding()
     }
+    .frame(maxWidth: .infinity, maxHeight: 103)
+    .background(Color(.systemBackground))
+    .cornerRadius(12)
+    .shadow(color: Color.gray.opacity(0.5), radius: 5)
+  }
 }
 
-#Preview {
-    TodaysTasksCardView()
+#Preview{
+  TodaysTasksCardView(
+    taskCategory: "work",
+    taskTitle: "Preparations",
+    taskDescription: "Worem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate.",
+    taskTime: "7:30 AM - 8:00 AM")
 }
