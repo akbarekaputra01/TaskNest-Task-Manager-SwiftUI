f16e1b103a9793319333eb1641185a0e85868e8f
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-06-13 01:07:40 +0700

    feat: update calendar view

--- TaskNest-Task-Manager-SwiftUI/Screens/Calendar/CalendarScreen.swift
+++ TaskNest-Task-Manager-SwiftUI/Screens/Calendar/CalendarScreen.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct CalendarScreen: View {
+  @State private var selectedDate = Date()
+
   var body: some View {
-    Text("Calendar Screen")
+    VStack(alignment: .leading) {
+      DatePicker(
+        "",
+        selection: $selectedDate,
+        displayedComponents: .date
+      )
+      .datePickerStyle(.graphical)
+      .padding(.horizontal)
+
+      Text("Tasks on \(DateUtils.formattedDDMMYYYY(selectedDate: selectedDate))")
+        .frame(width: .infinity, alignment: .leading)
+        .font(.title3)
+        .fontWeight(.semibold)
+        .padding(.horizontal)
+        .padding(.top)
+
+      ScrollView(.vertical, showsIndicators: false) {
+        VStack(alignment: .leading, spacing: 22) {
+          ForEach(
+            MockData.tasks.filter {
+              $0.taskDate == DateUtils.formattedDDMMYYYY(selectedDate: selectedDate)
+            }.prefix(3), id: \.id
+          ) {
+            task in
+            TodaysTasksCardView(tasks: task)
+          }
+        }
+        .padding(.vertical)
+        .padding(.horizontal)
+      }
+
+      Spacer()
+    }
   }
 }
 
 #Preview{
   CalendarScreen()
 }
+//
