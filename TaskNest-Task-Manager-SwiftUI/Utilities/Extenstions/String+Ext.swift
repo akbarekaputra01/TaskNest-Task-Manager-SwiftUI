3d027830bbf3a937cf835e59f8c34f891d43b7d5
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-06-15 23:23:37 +0700

    feat: add email format checker

---
+++ TaskNest-Task-Manager-SwiftUI/Utilities/Extenstions/String+Ext.swift
@@ -0,0 +1,52 @@
+//
+//  String+Ext.swift
+//  TaskNest-Task-Manager-SwiftUI
+//
+//  Created by Akbar Eka Putra on 15/06/24.
+//
+
+import Foundation
+import RegexBuilder
+
+extension String {
+
+  var isValidEmail: Bool {
+    // let emailFormat         = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    // let emailPredicate      = NSPredicate(format: "SELF MATCHES %@", emailFormat)
+    // return emailPredicate.evaluate(with: self)
+
+    let emailRegex = Regex {
+      // Local part
+      OneOrMore {
+        CharacterClass(
+          .anyOf("._%+-"),
+          ("A"..."Z"),
+          ("0"..."9"),
+          ("a"..."z")
+        )
+      }
+      "@"
+      // Domain part
+      OneOrMore {
+        CharacterClass(
+          ("A"..."Z"),
+          ("a"..."z"),
+          ("0"..."9")
+        )
+      }
+      Optionally {
+        OneOrMore {
+          "."
+          OneOrMore {
+            CharacterClass(
+              ("A"..."Z"),
+              ("a"..."z")
+            )
+          }
+        }
+      }
+    }
+
+    return self.wholeMatch(of: emailRegex) != nil
+  }
+}
