faa35d73345825c0043ad2e7853d70fdc738bd12
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-06-15 23:26:15 +0700

    feat: improve signup screen

--- TaskNest-Task-Manager-SwiftUI/Screens/SignUpView/SignUpView.swift
+++ TaskNest-Task-Manager-SwiftUI/Screens/SignUpView/SignUpView.swift
@@ -14,138 +14,185 @@ struct SigupView: View {
 
   @StateObject var viewModel = SignupViewModel()
 
+  @FocusState private var focusedTextField: FormTextField?
+  enum FormTextField {
+    case name, username, email, password, confirmPassword
+  }
+
   var body: some View {
     NavigationView {
-      VStack(alignment: .leading, spacing: 22) {
-        Spacer()
+      GeometryReader { geometry in
+        ScrollView {
+          VStack(alignment: .leading, spacing: 22) {
+            HStack {
+              Spacer()
+              Text("Sign Up")
+                .font(.largeTitle)
+                .bold()
+              Spacer()
+            }
 
-        HStack {
-          Spacer()
-          Text("Sign Up")
-            .font(.largeTitle)
-            .bold()
-          Spacer()
-        }
+            TextField("Name", text: $viewModel.user.name)
+              .padding()
+              .background(Color(.systemGray6))
+              .cornerRadius(10)
+              .autocapitalization(.none)
+              .disableAutocorrection(true)
+              .focused($focusedTextField, equals: .name)
+              .onSubmit { focusedTextField = .username }
+              .submitLabel(.next)
 
-        TextField("Email", text: $viewModel.email)
+            TextField("username", text: $viewModel.user.username)
+              .padding()
+              .background(Color(.systemGray6))
+              .cornerRadius(10)
+              .autocapitalization(.none)
+              .disableAutocorrection(true)
+              .focused($focusedTextField, equals: .username)
+              .onSubmit { focusedTextField = .email }
+              .submitLabel(.next)
+
+            TextField("Email", text: $viewModel.user.email)
+              .padding()
+              .background(Color(.systemGray6))
+              .cornerRadius(10)
+              .autocapitalization(.none)
+              .disableAutocorrection(true)
+              .focused($focusedTextField, equals: .email)
+              .onSubmit { focusedTextField = .password }
+              .submitLabel(.next)
+
+            SecureField("Password", text: $viewModel.user.password)
+              .padding()
+              .background(Color(.systemGray6))
+              .cornerRadius(10)
+              .autocapitalization(.none)
+              .disableAutocorrection(true)
+              .focused($focusedTextField, equals: .password)
+              .onSubmit { focusedTextField = .confirmPassword }
+              .submitLabel(.next)
+
+            SecureField("Password", text: $viewModel.confirmPassword)
+              .padding()
+              .background(Color(.systemGray6))
+              .cornerRadius(10)
+              .autocapitalization(.none)
+              .disableAutocorrection(true)
+              .focused($focusedTextField, equals: .confirmPassword)
+              .onSubmit { focusedTextField = nil }
+              .submitLabel(.continue)
+
+            HStack {
+              Button {
+                viewModel.isAgree.toggle()
+              } label: {
+                Image(systemName: viewModel.isAgree ? "checkmark.circle.fill" : "circle")
+              }
+              Text("Agree with Terms & Conditions")
+                .font(.caption2)
+              Spacer()
+            }
+
+            Button(action: {
+              viewModel.signup()
+            }) {
+              Text("Sign up")
+                .frame(maxWidth: .infinity)
+                .fontWeight(.bold)
+            }
+            .padding()
+            .disabled(
+              viewModel.user.email.isEmpty || viewModel.user.password.isEmpty
+                || viewModel.confirmPassword.isEmpty
+            )
+            .background(Color.accentColor)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .opacity(
+              viewModel.user.email.isEmpty
+                ? 0.5
+                : viewModel.user.password.isEmpty
+                  ? 0.5
+                  : viewModel.confirmPassword.isEmpty
+                    ? 0.5 : !viewModel.isAgree ? 0.5 : 1
+            )
+
+            HStack {
+              Rectangle()
+                .fill(Color.gray.opacity(0.5))
+                .frame(height: 1)
+              Text("Or log in with")
+                .foregroundColor(.gray.opacity(0.5))
+              Rectangle()
+                .fill(Color.gray.opacity(0.5))
+                .frame(height: 1)
+            }
+
+            HStack {
+              Spacer()
+              Button {
+                // Handle Apple sign in logic here
+              } label: {
+                Image("appleIcon")
+                  .frame(width: 5)
+                  .padding(20)
+                  .overlay(
+                    Circle()
+                      .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                  )
+              }
+              Button {
+                // Handle Google sign in logic here
+              } label: {
+                Image("googleIcon")
+                  .frame(width: 5)
+                  .padding(20)
+                  .overlay(
+                    Circle()
+                      .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                  )
+              }
+              .padding(.horizontal)
+              Button {
+                // Handle Facebook sign in logic here
+              } label: {
+                Image("facebookIcon")
+                  .frame(width: 5)
+                  .padding(20)
+                  .overlay(
+                    Circle()
+                      .stroke(Color.gray.opacity(0.5), lineWidth: 1)
+                  )
+              }
+              Spacer()
+            }
+
+            HStack {
+              Spacer()
+              Text("Already have Account?")
+              NavigationLink(destination: AccountView().navigationBarBackButtonHidden()) {
+                Text("Sign In")
+                  .fontWeight(.bold)
+              }
+              Spacer()
+            }
+          }
           .padding()
-          .background(Color(.systemGray6))
-          .cornerRadius(10)
-          .autocapitalization(.none)
-          .disableAutocorrection(true)
-
-        SecureField("Password", text: $viewModel.password)
-          .padding()
-          .background(Color(.systemGray6))
-          .cornerRadius(10)
-          .autocapitalization(.none)
-          .disableAutocorrection(true)
-
-        SecureField("Password", text: $viewModel.confirmPassword)
-          .padding()
-          .background(Color(.systemGray6))
-          .cornerRadius(10)
-          .autocapitalization(.none)
-          .disableAutocorrection(true)
-
-        HStack {
-          Button {
-            viewModel.isAgree.toggle()
-          } label: {
-            Image(systemName: viewModel.isAgree ? "checkmark.circle.fill" : "circle")
-          }
-          Text("Agree with Terms & Conditions")
-          Spacer()
         }
+        // To unable scroll the scrollview
+        // Note: scrollView used because after enter between TextField and SecureField, screen is like bounce
+        .environment(\.isScrollEnabled, false)
 
-        Button(action: {
-          viewModel.register()
-        }) {
-          Text("Sign up")
-            .frame(maxWidth: .infinity)
-            .fontWeight(.bold)
-        }
-        .padding()
-        .disabled(
-          viewModel.email.isEmpty || viewModel.password.isEmpty || viewModel.confirmPassword.isEmpty
-        )
-        .background(Color.accentColor)
-        .foregroundColor(.white)
-        .cornerRadius(10)
-        .opacity(
-          viewModel.email.isEmpty
-            ? 0.5
-            : viewModel.password.isEmpty
-              ? 0.5
-              : viewModel.confirmPassword.isEmpty
-                ? 0.5 : !viewModel.isAgree ? 0.5 : 1)
-
-        HStack {
-          Rectangle()
-            .fill(Color.gray.opacity(0.5))
-            .frame(height: 1)
-          Text("Or log in with")
-            .foregroundColor(.gray.opacity(0.5))
-          Rectangle()
-            .fill(Color.gray.opacity(0.5))
-            .frame(height: 1)
-        }
-
-        HStack {
-          Spacer()
-          Button {
-            // Handle Apple sign in logic here
-          } label: {
-            Image("appleIcon")
-              .frame(width: 5)
-              .padding(20)
-              .overlay(
-                Circle()
-                  .stroke(Color.gray.opacity(0.5), lineWidth: 1)
-              )
-          }
-          Button {
-            // Handle Google sign in logic here
-          } label: {
-            Image("googleIcon")
-              .frame(width: 5)
-              .padding(20)
-              .overlay(
-                Circle()
-                  .stroke(Color.gray.opacity(0.5), lineWidth: 1)
-              )
-          }
-          .padding(.horizontal)
-          Button {
-            // Handle Facebook sign in logic here
-          } label: {
-            Image("facebookIcon")
-              .frame(width: 5)
-              .padding(20)
-              .overlay(
-                Circle()
-                  .stroke(Color.gray.opacity(0.5), lineWidth: 1)
-              )
-          }
-          Spacer()
-        }
-
-        HStack {
-          Spacer()
-          Text("Already have Account?")
-          NavigationLink(destination: AccountView().navigationBarBackButtonHidden()) {
-            Text("Sign In")
-              .fontWeight(.bold)
-          }
-          Spacer()
-        }
-
-        Spacer()
+        // To limiting height the scrollView is just one screen
+        .frame(height: geometry.size.height)
       }
-
-      .padding()
     }
-
+    .alert(item: $viewModel.alertItem) { alertItem in
+      Alert(
+        title: alertItem.title,
+        message: alertItem.message,
+        dismissButton: alertItem.dismissButton)
+    }
   }
 }
 
--- TaskNest-Task-Manager-SwiftUI/Screens/SignUpView/SignupViewModel.swift
+++ TaskNest-Task-Manager-SwiftUI/Screens/SignUpView/SignupViewModel.swift
@@ -9,29 +9,63 @@ import Firebase
 import SwiftUI
 
 final class SignupViewModel: ObservableObject {
-  @Published var email = ""
-  @Published var password = ""
+  @AppStorage("user") private var userData: Data?
+
+  @Published var user = User()
+  @Published var alertItem: AlertItem?
+
   @Published var confirmPassword = ""
   @Published var isAgree = false
 
-  func register() {
-    guard isAgree == true else {
-      print("Please agree term and conditions.")
-      return
+  var isValidRegisterForm: Bool {
+    guard
+      !user.name.isEmpty && !user.username.isEmpty && !user.email.isEmpty && !user.password.isEmpty
+        && !self.confirmPassword.isEmpty
+    else {
+      alertItem = AlertContext.invalidForm
+      return false
     }
 
-    // Validate password and confirm password
-    guard password == confirmPassword else {
-      print("Passwords do not match.")
-      return
+    guard user.email.isValidEmail else {
+      alertItem = AlertContext.invalidEmail
+      return false
     }
 
-    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-      if let error = error {
-        print("Error registering user: \(error.localizedDescription)")
-      } else {
-        print("User registered successfully.")
+    guard user.password == self.confirmPassword else {
+      alertItem = AlertContext.invalidRegistPassword
+      return false
+    }
+
+    guard self.isAgree else {
+      alertItem = AlertContext.invalidAgreement
+      return false
+    }
+
+    return true
+  }
+
+  func signup() {
+    guard isValidRegisterForm else { return }
+
+    do {
+      let data = try JSONEncoder().encode(user)
+      userData = data
+
+      Auth.auth().createUser(withEmail: user.email, password: user.password) { authResult, error in
+        if let error = error {
+          print("Error registering user: \(error.localizedDescription)")
+          // Custom alert
+          self.alertItem = AlertItem(
+            title: Text("Error registering user"),
+            message: Text(error.localizedDescription),
+            dismissButton: .default(Text("OK")))
+        } else {
+          print("User registered successfully.")
+          self.alertItem = AlertContext.userRegistSuccess
+        }
       }
+    } catch {
+      alertItem = AlertContext.invalidUserData
     }
   }
 
