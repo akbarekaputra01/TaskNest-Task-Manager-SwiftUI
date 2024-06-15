a93c3a8755ae1ea29c70c54699a8f928fa81cdc4
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-06-15 14:31:34 +0700

    feat: add connection with firebase

--- TaskNest-Task-Manager-SwiftUI/TaskNest_Task_Manager_SwiftUIApp.swift
+++ TaskNest-Task-Manager-SwiftUI/TaskNest_Task_Manager_SwiftUIApp.swift
@@ -5,13 +5,40 @@
 //  Created by Akbar Eka Putra on 09/06/24.
 //
 
+//import SwiftUI
+//
+//@main
+//struct TaskNest_Task_Manager_SwiftUIApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
+
+import FirebaseCore
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 @main
-struct TaskNest_Task_Manager_SwiftUIApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
