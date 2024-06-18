4417cb4c5504bc8696ffb56f6a08d4fa10b5a9f8
akbarekaputra01 <akbarekaputra01@gmail.com>
2024-06-18 09:40:42 +0700

    feat: add home view in tabView

--- TaskNest-Task-Manager-SwiftUI/Views/TaskNestTabView/TaskNestTabView.swift
+++ TaskNest-Task-Manager-SwiftUI/Views/TaskNestTabView/TaskNestTabView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct TaskNestTabView: View {
   var body: some View {
     TabView {
-      SignUpView()
+      HomeView()
         .tabItem { Label("Home", systemImage: "house") }
       TasksView()
         .tabItem { Label("Tasks", systemImage: "list.clipboard") }
