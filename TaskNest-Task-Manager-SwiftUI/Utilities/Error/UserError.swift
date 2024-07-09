3f52764502c3637104142435e85e4a773dabfc67
Akbar Eka Putra <akbarekaputra01@gmail.com>
2024-07-09 21:28:10 +0700

    feat: added UserError enum for user validation errors

---
+++ TaskNest-Task-Manager-SwiftUI/Utilities/Error/UserError.swift
@@ -0,0 +1,13 @@
+//
+//  UserError.swift
+//  TaskNest-Task-Manager-SwiftUI
+//
+//  Created by Akbar Eka Putra on 09/07/24.
+//
+
+import Foundation
+
+enum UserError: Error {
+    case invalidUserData
+    case emailUsed
+}
