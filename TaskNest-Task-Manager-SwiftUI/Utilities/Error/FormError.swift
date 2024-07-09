52231afb4104fa92c63e5a4b94b85ece9c6b6ea6
Akbar Eka Putra <akbarekaputra01@gmail.com>
2024-07-09 21:26:16 +0700

    feat: added FormError enum for form validation errors

---
+++ TaskNest-Task-Manager-SwiftUI/Utilities/Error/FormError.swift
@@ -0,0 +1,15 @@
+//
+//  FormError.swift
+//  TaskNest-Task-Manager-SwiftUI
+//
+//  Created by Akbar Eka Putra on 09/07/24.
+//
+
+import Foundation
+
+enum FormError: Error {
+    case invalidForm
+    case invalidEmail
+    case invalidPassword
+    case invalidConfirmPassword
+}
